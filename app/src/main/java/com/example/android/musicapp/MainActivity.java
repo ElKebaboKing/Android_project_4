8788ffa6b0a0864036957d52fa9bcfb3a2062898
ElKebaboKing <ranielmaalouf@gmail.com>
2017-05-10 10:07:40 +0200

    Add intent for search, and code for handling intent.

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the Vew that shows the search page
+        TextView search = (TextView) findViewById(R.id.search);
+
+        // Set a click listener on that View
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open SearchActivity
+                Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+
+                // Start the new activity
+                startActivity(searchIntent);
+            }
+        });
     }
 }
