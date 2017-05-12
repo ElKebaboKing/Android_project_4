c4543b23e8f3c0c36ee2178e6d022838a2281884
ElKebaboKing <ranielmaalouf@gmail.com>
2017-05-12 10:19:50 +0200

    Add click listeners to the buttons, and move the image view to the end in order to avoid compile error.

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         TextView search = (TextView) findViewById(R.id.search_option);
         TextView browse = (TextView) findViewById(R.id.browse_option);
         TextView library = (TextView) findViewById(R.id.library_option);
+        Button previous = (Button) findViewById(R.id.previous);
+        Button play_pause = (Button) findViewById(R.id.play_pause);
+        Button next = (Button) findViewById(R.id.next);
 
         // Set a click listener for the search View
         search.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +58,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(libraryIntent);
             }
         });
+
+        // Set a click listener for the previous button
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a toast message describing the button that was pressed
+                Toast.makeText(getApplicationContext(),"Play previous track",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set a click listener for the play_pause button
+        play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a toast message describing the button that was pressed
+                Toast.makeText(getApplicationContext(),"Play/pause track",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set a click listener for the next button
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a toast message describing the button that was pressed
+                Toast.makeText(getApplicationContext(),"Play next track",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
